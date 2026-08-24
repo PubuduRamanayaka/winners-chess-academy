5a919afef47cacf6af44f81a9d1cc81d2649566d
PubuduRamanayaka <pubudumadhushan2005@gmail.com>
2026-08-25 00:28:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -595,3 +595,20 @@ if (deleteAccountButton) {
     );
 
 }
+
+const menuButton =
+    document.getElementById("menuButton");
+
+const navLinks =
+    document.getElementById("navLinks");
+
+
+if (menuButton && navLinks) {
+
+    menuButton.addEventListener("click", function() {
+
+        navLinks.classList.toggle("active");
+
+    });
+
+}
